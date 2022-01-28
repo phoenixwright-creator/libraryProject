777caffbf8355802f7a2998ba0b7540734a4e57f
phoenixwright-creator <phoenixwright725@gmail.com>
2022-01-28 16:08:21 +0100

    added an overlay to open a pop up form when clicking on add a book button + gave it more style and functionality as cancelling the request and setting some type inputs correctly

--- script.js
+++ script.js
@@ -14,21 +14,16 @@ Book.prototype.info = function () {
 };
 
 function addBookToLibrary() {
-  const title = prompt('Please enter the name of the book to add to the library : ').toUpperCase();
-  const author = prompt('Please enter the name of its author : ').toUpperCase();
-  const pages = prompt('Please enter the number of pages this book contains : ');
-  let read = prompt('Please enter if you read it or not (Y for Yes/N for No) : ').toUpperCase();
-  if(read === 'Y') {
-    read = 'YES';
-  }
-  else if(read === 'N') {
-    read = 'NO';
-  }
-  if (title !== null && author !== null && pages !== null) {
-    let book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-  }
-  displayLibrary(myLibrary);
+  const activateFormOverlay = document.getElementById('overlay');
+  activateFormOverlay.style.display = 'block';
+  const title = document.getElementById('formBookName');
+  const author = document.getElementById('formBookAuthor');
+  const pages = document.getElementById('formPagesNumber');
+  const read = document.getElementById('formRead');
+  title.value = "";
+  author.value = "";
+  pages.value = null;
+  read.checked = true;
 }
 
 function deleteBook(event) {
@@ -48,99 +43,102 @@ function changeReadStatus(event) {
 
 function displayLibrary(library) {
   const oldLibraryContent = document.getElementById('libraryContent');
-  console.log(oldLibraryContent);
-  if(oldLibraryContent !== null){
-    libraryTab.removeChild(oldLibraryContent);
+  if(oldLibraryContent === null){
+    const newLibraryContent = document.createElement('div');
+    newLibraryContent.id = 'libraryContent';
   }
+  else if(oldLibraryContent !== null){
+    libraryTab.removeChild(oldLibraryContent);
+    const newLibraryContent = document.createElement('div');
+    newLibraryContent.id = 'libraryContent';
+    if(library.length !== 0){
+      const defaultLine = document.createElement('div');
+      defaultLine.id = 'defaultLine';
+      defaultLine.className = 'bookLine';
 
-  const newLibraryContent = document.createElement('div');
-  newLibraryContent.id = 'libraryContent';
+      const nameDiv = document.createElement('div');
+      nameDiv.id = 'nameDiv';
+      nameDiv.className = 'nameDiv cell color1';
+      nameDiv.innerHTML = 'NAME';
+      defaultLine.appendChild(nameDiv);
 
-  const defaultLine = document.createElement('div');
-  defaultLine.id = 'defaultLine';
-  defaultLine.className = 'bookLine';
+      const authorDiv = document.createElement('div');
+      authorDiv.id = 'authorDiv';
+      authorDiv.className = 'authorDiv cell color1';
+      authorDiv.innerHTML = 'AUTHOR';
+      defaultLine.appendChild(authorDiv);
 
-  const nameDiv = document.createElement('div');
-  nameDiv.id = 'nameDiv';
-  nameDiv.className = 'nameDiv cell color1';
-  nameDiv.innerHTML = 'NAME';
-  defaultLine.appendChild(nameDiv);
+      const pagesDiv = document.createElement('div');
+      pagesDiv.id = 'pagesDiv';
+      pagesDiv.className = 'pagesDiv cell color1';
+      pagesDiv.innerHTML = 'PAGES';
+      defaultLine.appendChild(pagesDiv);
 
-  const authorDiv = document.createElement('div');
-  authorDiv.id = 'authorDiv';
-  authorDiv.className = 'authorDiv cell color1';
-  authorDiv.innerHTML = 'AUTHOR';
-  defaultLine.appendChild(authorDiv);
+      const statusDiv = document.createElement('div');
+      statusDiv.id = 'statusDiv';
+      statusDiv.className = 'statusDiv cell color1';
+      statusDiv.innerHTML = 'READ';
+      defaultLine.appendChild(statusDiv);
 
-  const pagesDiv = document.createElement('div');
-  pagesDiv.id = 'pagesDiv';
-  pagesDiv.className = 'pagesDiv cell color1';
-  pagesDiv.innerHTML = 'PAGES';
-  defaultLine.appendChild(pagesDiv);
+      newLibraryContent.appendChild(defaultLine);
 
-  const statusDiv = document.createElement('div');
-  statusDiv.id = 'statusDiv';
-  statusDiv.className = 'statusDiv cell color1';
-  statusDiv.innerHTML = 'READ';
-  defaultLine.appendChild(statusDiv);
+      for (let book in library) {
+        const newBook = document.createElement('div');
+        newBook.id = Number(book) + 1;
+        newBook.className = 'bookLine';
 
-  newLibraryContent.appendChild(defaultLine);
-    
-    for (let book in library) {
-      const newBook = document.createElement('div');
-      newBook.id = Number(book) + 1;
-      newBook.className = 'bookLine';
+        const bookName = document.createElement('div');
+        bookName.id = 'bookName';
+        bookName.className = 'bookName cell color2';
+        bookName.innerHTML = library[book].title;
+        newBook.appendChild(bookName);
 
-      const bookName = document.createElement('div');
-      bookName.id = 'bookName';
-      bookName.className = 'bookName cell color2';
-      bookName.innerHTML = library[book].title;
-      newBook.appendChild(bookName);
+        const authorName = document.createElement('div');
+        authorName.id = 'authorName';
+        authorName.className = 'authorName cell color2';
+        authorName.innerHTML = library[book].author;
+        newBook.appendChild(authorName);
 
-      const authorName = document.createElement('div');
-      authorName.id = 'authorName';
-      authorName.className = 'authorName cell color2';
-      authorName.innerHTML = library[book].author;
-      newBook.appendChild(authorName);
+        const pagesNumber = document.createElement('div');
+        pagesNumber.id = 'pagesNumber';
+        pagesNumber.className = 'pagesNumber cell color2';
+        pagesNumber.innerHTML = library[book].pages;
+        newBook.appendChild(pagesNumber);
 
-      const pagesNumber = document.createElement('div');
-      pagesNumber.id = 'pagesNumber';
-      pagesNumber.className = 'pagesNumber cell color2';
-      pagesNumber.innerHTML = library[book].pages;
-      newBook.appendChild(pagesNumber);
+        const readStatus = document.createElement('div');
+        readStatus.id = 'readStatus';
+        readStatus.className = 'readStatus cell color2';
+        readStatus.innerHTML = library[book].read;
+        newBook.appendChild(readStatus);
 
-      const readStatus = document.createElement('div');
-      readStatus.id = 'readStatus';
-      readStatus.className = 'readStatus cell color2';
-      readStatus.innerHTML = library[book].read;
-      newBook.appendChild(readStatus);
+        const changeStatusButton = document.createElement('button');
+        changeStatusButton.id = newBook.id;
+        changeStatusButton.className = 'changeReadStatus cell info';
+        changeStatusButton.innerHTML = 'CHANGE READ STATUS';
+        newBook.appendChild(changeStatusButton);
 
-      const changeStatusButton = document.createElement('button');
-      changeStatusButton.id = newBook.id;
-      changeStatusButton.className = 'changeReadStatus cell info';
-      changeStatusButton.innerHTML = 'CHANGE READ STATUS';
-      newBook.appendChild(changeStatusButton);
+        const deleteBookButton = document.createElement('button');
+        deleteBookButton.id = newBook.id;
+        deleteBookButton.className = 'deleteBook cell danger';
+        deleteBookButton.innerHTML = 'DELETE BOOK';
+        newBook.appendChild(deleteBookButton);
 
-      const deleteBookButton = document.createElement('button');
-      deleteBookButton.id = newBook.id;
-      deleteBookButton.className = 'deleteBook cell danger';
-      deleteBookButton.innerHTML = 'DELETE BOOK';
-      newBook.appendChild(deleteBookButton);
-
-      newLibraryContent.appendChild(newBook);
-      
+        newLibraryContent.appendChild(newBook);
+        
+      }
     }
-
+    
     libraryTab.appendChild(newLibraryContent);
 
-  const readStatusButtons = document.querySelectorAll('.changeReadStatus');
-  for(let i=0; i<readStatusButtons.length; i++) {
-    readStatusButtons[i].addEventListener('click', changeReadStatus);
-  }
-
-  const deleteButtons = document.querySelectorAll('.deleteBook');
-  for(let i=0; i<deleteButtons.length; i++) {
-    deleteButtons[i].addEventListener('click', deleteBook);
+    const readStatusButtons = document.querySelectorAll('.changeReadStatus');
+    for(let i=0; i<readStatusButtons.length; i++) {
+      readStatusButtons[i].addEventListener('click', changeReadStatus);
+    }
+  
+    const deleteButtons = document.querySelectorAll('.deleteBook');
+    for(let i=0; i<deleteButtons.length; i++) {
+      deleteButtons[i].addEventListener('click', deleteBook);
+    }
   }
 }
 
@@ -150,4 +148,36 @@ addBookButton.id = 'addBookButton';
 
 libraryTab.appendChild(addBookButton);
 
+const libraryContent = document.createElement('div');
+libraryContent.id = 'libraryContent';
+
+libraryTab.appendChild(libraryContent);
+
 addBookButton.addEventListener('click', addBookToLibrary);
+
+const addBookFormButton = document.getElementById('addBook');
+  addBookFormButton.addEventListener('click', () => {
+    const activateFormOverlay = document.getElementById('overlay');
+    const title = document.getElementById('formBookName').value.toUpperCase();
+    const author = document.getElementById('formBookAuthor').value.toUpperCase();
+    const pages = document.getElementById('formPagesNumber').value;
+    const read = document.getElementById('formRead');
+
+    if (title !== "" && author !== "" && pages !== null && read.checked === true) {
+      let book = new Book(title, author, pages, 'YES');
+      myLibrary.push(book);
+    }
+    else if (title !== "" && author !== "" && pages !== null && read.checked === false) {
+      let book = new Book(title, author, pages, 'NO');
+      myLibrary.push(book);
+    }
+    activateFormOverlay.style.display = 'none';
+    displayLibrary(myLibrary);
+  });
+
+  const cancelFormButton = document.getElementById('cancel');
+  cancelFormButton.addEventListener('click', () => {
+    const activateFormOverlay = document.getElementById('overlay');
+    activateFormOverlay.style.display = 'none';
+  });
+
